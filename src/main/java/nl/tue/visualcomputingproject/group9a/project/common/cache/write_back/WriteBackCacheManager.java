78da585286a3e581707ee253849b1bcee37064dd
Kaj0Wortel <kaj.wortel@gmail.com>
2021-02-26 17:21:01 +0100

    Implemented cache indexing for WriteBackCacheManager

--- src/main/java/nl/tue/visualcomputingproject/group9a/project/common/cache/write_back/WriteBackCacheManager.java
+++ src/main/java/nl/tue/visualcomputingproject/group9a/project/common/cache/write_back/WriteBackCacheManager.java
@@ -408,64 +408,91 @@ public class WriteBackCacheManager<T extends CacheableObject>
 
 	@Override
 	public void indexCache(FileIdFactory<? extends FileId> idFactory) {
-//		try {
-//			LOGGER.info("Indexing disk cache...");
-//			if (!cacheDir.exists() && !cacheDir.mkdirs()) {
-//				LOGGER.error("Could not create cache directory: " + cacheDir);
-//				LOGGER.error("Continuing without disk cache.");
-//				return;
-//			}
-//
-//			if (!Files.isDirectory(cacheDir.toPath())) {
-//				LOGGER.error("Disk cache directory is not a directory");
-//				LOGGER.error("Continuing without disk cache.");
-//				return;
-//			}
-//
-//			String cachePath = cacheDir.getAbsolutePath();
-//			int rootDirLength = cachePath.length() +
-//					(cachePath.endsWith(File.separator)
-//							? 0
-//							: File.separator.length());
-//			File[] fileArr = cacheDir.listFiles();
-//			if (fileArr == null) return;
-//			List<File> files = new ArrayList<>(Arrays.asList(fileArr));
-//			for (int i = 0; i < files.size(); i++) {
-//				File file = files.get(i);
-//				if (file.isDirectory()) {
-//					fileArr = file.listFiles();
-//					if (fileArr != null) {
-//						files.addAll(Arrays.asList(fileArr));
-//					}
-//
-//				} else if (file.getName().endsWith(Settings.CACHE_EXT)) {
-//					String path = file.getAbsolutePath();
-//					String name = path.substring(
-//							rootDirLength,
-//							path.length() - Settings.CACHE_EXT.length());
-//					try {
-//						FileId id = idFactory.fromPath(name);
-//						if (id != null) {
-//							track(id);
-//							LOGGER.info("Added " + name + " to disk cache!");
-//						}
-//					} catch (Exception e) {
-//						LOGGER.info("Exception occurred while indexing " + name);
-//						e.printStackTrace();
-//					}
-//				} else if (file.getName().endsWith(Settings.TMP_CACHE_EXT)) {
-//					// Delete old temporary cache files if found.
-//					//noinspection ResultOfMethodCallIgnored
-//					file.delete();
-//				}
-//			}
-//
-//			LOGGER.info("Finished indexing disk cache!");
-//
-//		} catch (Exception e) {
-//			LOGGER.error("Aborted indexing disk cache!");
-//			e.printStackTrace();
-//		}
+		try {
+			LOGGER.info("Indexing disk cache...");
+			if (!cacheDir.exists() && !cacheDir.mkdirs()) {
+				LOGGER.error("Could not create cache directory: " + cacheDir);
+				LOGGER.error("Continuing without disk cache.");
+				return;
+			}
+
+			if (!Files.isDirectory(cacheDir.toPath())) {
+				LOGGER.error("Disk cache directory is not a directory");
+				LOGGER.error("Continuing without disk cache.");
+				return;
+			}
+
+			String cachePath = cacheDir.getAbsolutePath();
+			int rootDirLength = cachePath.length() +
+					(cachePath.endsWith(File.separator)
+							? 0
+							: File.separator.length());
+			File[] fileArr = cacheDir.listFiles();
+			if (fileArr == null) return;
+			List<File> files = new ArrayList<>(Arrays.asList(fileArr));
+			List<FileId> ids = new ArrayList<>();
+			for (int i = 0; i < files.size(); i++) {
+				File file = files.get(i);
+				if (file.isDirectory()) {
+					fileArr = file.listFiles();
+					if (fileArr != null) {
+						files.addAll(Arrays.asList(fileArr));
+					}
+
+				} else if (file.getName().endsWith(Settings.CACHE_EXT)) {
+					String path = file.getAbsolutePath();
+					String name = path.substring(
+							rootDirLength,
+							path.length() - Settings.CACHE_EXT.length());
+					try {
+						FileId id = idFactory.fromPath(name);
+						if (id != null) {
+							ids.add(id);
+							LOGGER.info("Added " + name + " to disk cache!");
+						}
+					} catch (Exception e) {
+						LOGGER.info("Exception occurred while indexing " + name);
+						e.printStackTrace();
+					}
+				} else if (file.getName().endsWith(Settings.TMP_CACHE_EXT)) {
+					// Delete old temporary cache files if found.
+					//noinspection ResultOfMethodCallIgnored
+					file.delete();
+				}
+			}
+
+			lock.lock();
+			try {
+				for (FileId id : ids) {
+					WriteBackClaimElem elem = claimMap.get(id);
+					if (elem != null) {
+						continue;
+					}
+					elem = new WriteBackClaimElem();
+					elem.stable++;
+					claimMap.put(id, elem);
+					elem.fileWriteClaim = new WriteBackReadWriteFilePolicyCacheClaim<T>(
+							id,
+							cacheDir);
+					
+					lock.unlock();
+					try {
+						filePolicy.track(id, this, elem.fileWriteClaim);
+					} finally {
+						lock.lock();
+					}
+				}
+				
+			} finally {
+				lock.unlock();
+			}
+
+			LOGGER.info("Finished indexing disk cache!");
+
+		} catch (Exception e) {
+			LOGGER.error("Aborted indexing disk cache!");
+			e.printStackTrace();
+		}
 	}
 	
 	private void invalidateClaim(ReadCacheClaim claim) {
