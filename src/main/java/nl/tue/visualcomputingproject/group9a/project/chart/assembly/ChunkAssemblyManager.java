b4b0243d8bf6153a22f4a8dee4e935dc3e105e6f
Kaj0Wortel <kaj.wortel@gmail.com>
2021-03-28 16:34:40 +0200

    Improved logic

--- src/main/java/nl/tue/visualcomputingproject/group9a/project/chart/assembly/ChunkAssemblyManager.java
+++ src/main/java/nl/tue/visualcomputingproject/group9a/project/chart/assembly/ChunkAssemblyManager.java
@@ -5,6 +5,7 @@ import com.google.common.eventbus.Subscribe;
 import nl.tue.visualcomputingproject.group9a.project.chart.ChartingModule;
 import nl.tue.visualcomputingproject.group9a.project.chart.events.PartialChunkAvailableEvent;
 import nl.tue.visualcomputingproject.group9a.project.chart.wfs.MapSheet;
+import nl.tue.visualcomputingproject.group9a.project.common.Settings;
 import nl.tue.visualcomputingproject.group9a.project.common.chunk.Chunk;
 import nl.tue.visualcomputingproject.group9a.project.common.chunk.ChunkId;
 import nl.tue.visualcomputingproject.group9a.project.common.chunk.PointCloudChunkData;
@@ -17,13 +18,14 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SuppressWarnings("UnstableApiUsage")
 public class ChunkAssemblyManager {
 	/** The logger of this class. */
 	static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	
-	private final Map<ChunkId, ChunkAssemblyJob> assemblyRequests = new HashMap<>();
+	private final Map<ChunkId, ChunkAssemblyJob> assemblyRequests = new ConcurrentHashMap<>();
 	private final EventBus eventBus;
 	
 	public ChunkAssemblyManager(EventBus eventBus) {
@@ -32,54 +34,58 @@ public class ChunkAssemblyManager {
 		logger.info("Chunk assembly manager ready!");
 	}
 	
-	public void assembleChunkRequest(ChunkId chunk, Collection<MapSheet> sheets) {
-		synchronized (assemblyRequests) {
-			if (assemblyRequests.containsKey(chunk)) {
+	public void assembleChunkRequest(final ChunkId chunk, Collection<MapSheet> sheets) {
+		assemblyRequests.compute(chunk, (key, old) -> {
+			if (old != null) {
 				throw new IllegalStateException("Attempted to request assembly of chunk that's already being assembled.");
 			}
-			
-			ChunkAssemblyJob job = new ChunkAssemblyJob(chunk, new HashSet<>(sheets));
-			assemblyRequests.put(chunk, job);
-			
-			logger.info("New assembly request! {}", job);
-		}
+			return new ChunkAssemblyJob(chunk, new HashSet<>(sheets));
+		});
+//		synchronized (assemblyRequests) {
+//			if (assemblyRequests.containsKey(chunk)) {
+//				throw new IllegalStateException("Attempted to request assembly of chunk that's already being assembled.");
+//			}
+//			
+//			ChunkAssemblyJob job = new ChunkAssemblyJob(chunk, new HashSet<>(sheets));
+//			assemblyRequests.put(chunk, job);
+//			
+//			logger.info("New assembly request! {}", job);
+//		}
 	}
 	
 	@Subscribe
 	public void partialChunkAvailable(PartialChunkAvailableEvent event) {
-		ChunkAssemblyJob job;
-		synchronized (assemblyRequests) {
-			if (!assemblyRequests.containsKey(event.getChunk().getChunkId())) {
-				throw new IllegalStateException("Chunk data provided for job that is not in progress!");
-			}
-			
-			//Lookup the job.
-			job = assemblyRequests.get(event.getChunk().getChunkId());
-			
-			logger.info("Partial chunk received for chunk {}.", job.getChunkId());
-			
-			//Register the partial chunk with the job.
-			job.newPartialChunk(event.getChunk(), event.getSheet());
-			
-			//If we can assemble this now, remove it from the map.
-			if (job.isReadyForAssembly()) {
-				assemblyRequests.remove(event.getChunk().getChunkId());
-			}
+		// Retrieve the job.
+		ChunkAssemblyJob job = assemblyRequests.get(event.getChunk().getChunkId());
+		if (job == null) {
+			throw new IllegalStateException("Chunk data provided for job that is not in progress!");
 		}
+		logger.info("Partial chunk received for chunk {}.", job.getChunkId());
 		
-		//Do assembly/posting if needed.
-		if (job.isReadyForAssembly()) {
+		// Add the sub-chunk to the job, and remove
+		// the job from the map if it is ready for assembly.
+		// Notice that a job cannot be removed if it has pending chunks,
+		// i.e. only the last partial chunk event can remove it.
+		boolean isReadyForAssembly = (assemblyRequests.computeIfPresent(event.getChunk().getChunkId(), (id, j) -> {
+			j.newPartialChunk(event.getChunk(), event.getSheet());
+			return (j.isReadyForAssembly() ? null : j);
+		}) == null);
+		
+		// Do assembly / posting only if needed.
+		if (!isReadyForAssembly) return;
+		
+		Settings.executorService.submit(() -> {
 			logger.info("Chunk {} is ready for assembly!", job.getChunkId());
 			
 			Chunk<ChunkId, PointCloudChunkData> assembledChunk = event.getChunk();
 			
 			if (job.getNumberOfPartialChunks() > 1) {
-				//This chunk needs assembly.
+				// This chunk needs assembly.
 				assembledChunk = job.assembleChunk();
 			}
 			
 			eventBus.post(new ChartChunkLoadedEvent(assembledChunk));
-		}
+		});
 	}
 	
 	
