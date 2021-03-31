0a7317626a7fa06aa6538e3c7d8235735645a7d9
Roelf Wichertjes <r.wichertjes@student.tue.nl>
2021-03-31 11:07:23 +0200

    Missing equals and hashcode.

--- src/main/java/nl/tue/visualcomputingproject/group9a/project/common/event/ChartTextureAvailableEvent.java
+++ src/main/java/nl/tue/visualcomputingproject/group9a/project/common/event/ChartTextureAvailableEvent.java
@@ -1,11 +1,13 @@
 package nl.tue.visualcomputingproject.group9a.project.common.event;
 
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 import nl.tue.visualcomputingproject.group9a.project.common.TextureType;
 import nl.tue.visualcomputingproject.group9a.project.common.chunk.ChunkPosition;
 
 import java.nio.ByteBuffer;
 
+@EqualsAndHashCode(callSuper = true)
 @Value
 public class ChartTextureAvailableEvent
 		extends AbstractEvent {
